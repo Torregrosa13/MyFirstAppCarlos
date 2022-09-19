48b44f4076f4e8bb5edc04e3de6c18487a4421e6
Carlos Garcia <garciatorregrosacarlos@gmail.com>
2022-09-19 20:32:38 +0200

    Init project

---
+++ MyFirstAppCarlos/MyFirstAppCarlos/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MyFirstAppCarlos
+//
+//  Created by Alumnos on 19/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+    
+
+}
+
